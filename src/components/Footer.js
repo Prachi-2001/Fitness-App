45623b63fc7562a4828a7b4cb707c747fcc53808
Prachi-2001 <prachipachang164@gmail.com>
2023-08-12 20:21:42 +0530

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import { Box, Stack, Typography } from '@mui/material';
-import Logo from '../assets/images/Logo-1.png';
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Logo1 from "../assets/images/Logo.png";
+import Logo2 from "../assets/images/flexzone-logo.png";
 
 const Footer = () => (
   <Box mt="80px" bgcolor="#FFF3F4">
-    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
-      {/* <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} /> */}
+    <Stack
+      sx={{ alignItems: "center" }}
+      flexWrap="wrap"
+      pt="24px"
+    >
+      <Stack gap="10px" sx={{display: "flex", flexDirection: "row"}}>
+        <img src={Logo1} alt="logo" style={{ width: "50px", height: "41px" }} />
+        <img
+          src={Logo2}
+          alt="logo"
+          style={{ width: "100px", height: "41px" }}
+        />
+      </Stack>
     </Stack>
-    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made by ❤️ Prachi</Typography>
+    <Typography
+      variant="h5"
+      sx={{ fontSize: { lg: "28px", xs: "20px" } }}
+      mt="41px"
+      textAlign="center"
+      pb="40px"
+    >
+      Made by ❤️ Prachi
+    </Typography>
   </Box>
 );
 
